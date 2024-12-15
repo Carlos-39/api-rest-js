50362d68e22fc7bc367dafafb3c8c60e404bf8b5
Carlos Daniel Corrales <carlox59681@gmail.com>
2024-12-15 00:16:31 -0500

    consumiendo api para mostrar peliculas en tendencias

--- src/main.js
+++ src/main.js
@@ -1,1 +1,30 @@
-console.log('hola' + API_KEY)
+// conseguir peliculas en tendencia de la API para el home
+async function getTrendingMoviesPreview(){
+	// llamar a la API en la parte de peliculas en tendencia diaria
+	const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY)
+
+	const data = await res.json()
+
+	const movies = data.results
+
+	console.log({data, movies})
+
+	// crear el espacio en el DOM por cada pelicula que haya
+	movies.forEach(movie => {
+		const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList')
+
+		const movieContainer = document.createElement('div')
+		movieContainer.classList.add('movie-container')
+
+		const movieIMG = document.createElement('img')
+		movieIMG.classList.add('movie-img')
+		movieIMG.setAttribute('alt', movie.title)
+		movieIMG.setAttribute('src', 'https://image.tmdb.org/t/p/w300' + movie.poster_path)
+
+		// renderizar en el DOM
+		movieContainer.appendChild(movieIMG)
+		trendingPreviewMoviesContainer.appendChild(movieContainer)
+	})
+}
+
+getTrendingMoviesPreview()
