31081a9bb950f9a47250d37058c7dd3cd072b7d5
Carlos Daniel Corrales <carlox59681@gmail.com>
2024-12-15 11:50:14 -0500

    generos de peliculas para el home implementados

--- src/main.js
+++ src/main.js
@@ -27,4 +27,35 @@ async function getTrendingMoviesPreview(){
 	})
 }
 
+// conseguir categorías de las películas de la API para el home
+async function getCategoriesMoviesPreview(){
+	// llamar a la API en la parte de genero de películas
+	const res = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=' + API_KEY + '&language=es')
+
+	const data = await res.json()
+
+	const categories = data.genres
+
+	console.log({data, categories})
+
+	// crear el espacio en el DOM por cada genero que haya
+	categories.forEach(category => {
+		const categoryPreviewMoviesContainer = document.querySelector('#categoriesPreview .categoriesPreview-list')
+
+		const categoryContainer = document.createElement('div')
+		categoryContainer.classList.add('category-container')
+
+		const categoryTitle = document.createElement('h3')
+		categoryTitle.classList.add('category-title')
+		categoryTitle.setAttribute('id', 'id' + category.id)
+		const categoryTitleText = document.createTextNode(category.name)
+
+		// renderizar en el DOM
+		categoryTitle.appendChild(categoryTitleText)
+		categoryContainer.appendChild(categoryTitle)
+		categoryPreviewMoviesContainer.appendChild(categoryContainer)
+	})
+}
+
 getTrendingMoviesPreview()
+getCategoriesMoviesPreview()
